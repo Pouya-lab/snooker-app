5936017960e3b73e15133e277978d04d304de58d
pouya <pouya_behrooj@yahoo.com>
2022-12-24 17:12:20 +0330

    creating home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
+import '../style/global.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
 }
